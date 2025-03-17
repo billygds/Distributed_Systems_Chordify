cd9ab645bacf2867b7bd056c963b462c39ef1494
ntua-el19913 <el19913@mail.ntua.gr>
2025-03-17 20:49:38 +0200

    added better description for help message

--- Chord_Client.py
+++ Chord_Client.py
@@ -29,28 +29,6 @@ def signal_handler(sig, frame):
 # Capture Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-def print_help():
-    """Εκτυπώνει βοήθεια με τις διαθέσιμες εντολές"""
-    help_text = """
-    Chordify Client - Available Commands:
-
-    insert <key> <value>   - Εισάγει ένα νέο (key, value) ζεύγος στον DHT.
-    delete <key>           - Διαγράφει το (key, value) ζεύγος με κλειδί <key>.
-    query <key>            - Αναζητά την τιμή του <key>. Αν <key> είναι '*', επιστρέφει όλα τα αποθηκευμένα δεδομένα.
-    depart <node_id>       - Ο κόμβος με αναγνωριστικό <node_id> αποχωρεί από το δίκτυο.
-    overlay               - Εμφανίζει την τρέχουσα τοπολογία του Chord δακτυλίου.
-    help                  - Εμφανίζει αυτό το μήνυμα βοήθειας.
-
-    Παράδειγμα χρήσης:
-    python testClient.py insert "song1" "node3"
-    python testClient.py query "song1"
-    python testClient.py delete "song1"
-    python testClient.py depart 5
-    python testClient.py overlay
-    python testClient.py help
-    """
-    print(help_text)
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python testClient.py <command> [<key>] [<value>]")
@@ -60,9 +38,6 @@ if __name__ == "__main__":
     ip = "127.0.0.1"
     port = 5000
     command = sys.argv[1]
-    if command == "help":
-        print_help()
-        sys.exit(0)
     key = sys.argv[2] if len(sys.argv) > 2 else None
     value = sys.argv[3] if len(sys.argv) > 3 else None
 
@@ -75,6 +50,32 @@ if __name__ == "__main__":
         response = send_request(ip, port, "depart", value=node_id)
     elif command == "insert":
         response = response = send_request(ip, port, command, key)
+    elif command == "help":
+        """Εκτυπώνει βοήθεια με τις διαθέσιμες εντολές"""
+        help_text = """
+        Chordify Client - Available Commands:
+
+        insert <key>        - Εισάγει ένα νέο (key, value) ζεύγος στον DHT.
+        delete <key>           - Διαγράφει το (key, value) ζεύγος με κλειδί <key>.
+        query <key>            - Αναζητά την τιμή του <key>. Αν <key> είναι '*', επιστρέφει όλα τα αποθηκευμένα δεδομένα.
+        join <ip> <port> ( <bootstra_ip> <bootstrap port> )     -Δημιουργεί ενα Chord Ring με τις δωσμένες πληροφορίες ή εισχωρεί έναν κόμβο
+                                                                 σε Chord Ring αν δωθεί η ip και port του bootstrap
+        depart <node_id>       - Ο κόμβος με αναγνωριστικό <node_id> αποχωρεί από το δίκτυο.
+        overlay               - Εμφανίζει την τρέχουσα τοπολογία του Chord δακτυλίου.
+        help                  - Εμφανίζει αυτό το μήνυμα βοήθειας.
+
+        Παράδειγμα χρήσης:
+        python Chord_Client.py insert "song"
+        python Chord_Client.py query "song"
+        python Chord_Client.py delete "song"
+        python Chord_Client.py join 127.0.0.1 5000 - Starts Chord Ring
+        python Chord_Client.py join 127.0.0.1 5001 127.0.0.1 5000 - Adds node to existing chord ring
+        python Chord_Client.py depart "5604500"
+        python Chord_Client.py overlay
+        python Chord_Client.py help
+        """
+        print(help_text)
+        sys.exit(0)
     else:
         response = send_request(ip, port, command, key, value)
 
